cd65b40e0f38a7445c84808ddca1e8714195040e
Alex Bates <Alex.Bates@nielseniq.com>
2024-01-05 12:22:17 -0500

    remove old cli handler

--- src/main/kotlin/cli/CliFunctions.kt
+++
@@ -1,9 +0,0 @@
-package cli
-
-fun parseArguments(args: Array<String>): CliArguments {
-    val organizedArgs = ArgsContainer(args)
-    return CliArguments(
-        configFilePath = organizedArgs.arguments["config"]?:"",
-        commandBlock = organizedArgs.arguments["command"]?:"",
-    )
-}
